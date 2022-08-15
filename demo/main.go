45508ecdcde363ae7a3eb2d3e601ac765a0c724e
Kunal Verma <vkunal321@gmail.com>
2022-08-15 14:17:28 +0530

    Add: Initial setup

---
+++ demo/main.go
@@ -0,0 +1,16 @@
+package main
+
+/*
+import (
+	"context"
+	"fmt"
+	"log"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go/service/ecr"
+)
+*/
+
+func main() {
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module kubescape-ecr-adaptor
+
+go 1.18
