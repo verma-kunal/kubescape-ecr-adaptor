3162125952f2a94bec602567b165ad9329d1b446
Kunal Verma <vkunal321@gmail.com>
2022-08-25 12:47:24 +0530

    Save changes

--- demo/main.go
+++ demo/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ecr"
+	"github.com/aws/aws-sdk-go-v2/service/ecr/types"
+	"github.com/aws/aws-sdk-go/aws"
+	"log"
+)
+
 /*
 import (
 	"context"
@@ -7,10 +17,41 @@ import (
 	"log"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go/service/ecr"
+	"github.com/aws/aws-sdk-go-v2/service/ecr"
 )
 */
 
+// vulnerabilityOut returns the scan results
+func vulnerabilityOut(output *ecr.DescribeImageScanFindingsOutput) {
+
+	fmt.Println(*output.ImageScanFindings.Findings[1].Name)
+
+}
+
 func main() {
+
+	ctx := context.TODO()
+	// Loading the AWS shared configuration:
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithSharedConfigProfile("default"))
+	if err != nil {
+		log.Fatalf("failed to load configuration, %v", err)
+	}
+
+	// Creating an ECR service client:
+	client := ecr.NewFromConfig(cfg)
+
+	scanFindings, err := client.DescribeImageScanFindings(ctx, &ecr.DescribeImageScanFindingsInput{
+		ImageId: &types.ImageIdentifier{
+			ImageDigest: aws.String("sha256:5045e645eb8963b389d773acd5b769020a0ad893564484c3bd9554186104c6d3"),
+			ImageTag:    aws.String("latest"),
+		},
+		RepositoryName: aws.String("hello-world"),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// calling out the results here:
+	vulnerabilityOut(scanFindings)
 
 }
